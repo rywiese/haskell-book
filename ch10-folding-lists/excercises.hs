04abdb97d82bd2b070dc11305182926e47dc4af1
Ry Wiese <rywiese@mn-rwiese-m.ad.smartthings.com>
2021-04-20 00:29:44 -0500

    Working on 10.6

---
+++ ch10-folding-lists/excercises.hs
@@ -0,0 +1,23 @@
+module Excercises where
+
+-- Understanding folds
+
+-- 1 b, c
+
+-- 3 c
+
+-- 4 a
+
+-- 5
+a = foldr (++) "" ["woot", "WOOT", "woot"]
+b = foldr max 'a' "fear is the little death"
+c = foldr (&&) True [False, True]
+d = foldr (||) False [False, True]
+e = foldl (flip ((++) . show)) "" [1..5]
+f = foldr (flip const) 'a' [1..5]
+g = foldl const 0 "tacos"
+h = foldr (flip const) 0 "burritos"
+i = foldr (flip const) 'z' [1..5]
+
+
+-- Database processing
