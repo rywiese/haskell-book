45ec85c34b0e5f410650cc3f1affd60b5825cdc2
Ry Wiese <rywiese@mn-rwiese-m.ad.smartthings.com>
2021-04-21 22:37:54 -0500

    Rest of 10.6

--- ch10-folding-lists/database.hs
+++ ch10-folding-lists/database.hs
@@ -13,6 +13,7 @@ theDatabase =
     , DbNumber 9001
     , DbString "Hello, world!"
     , DbDate (UTCTime (fromGregorian 1921 5 1) (secondsToDiffTime 34123))
+    , DbNumber 9003
     ]
 
 filterDbDate :: [DatabaseItem] -> [UTCTime]
@@ -22,6 +23,12 @@ filterDbNumber :: [DatabaseItem] -> [Integer]
 filterDbNumber dbItems = [ x | (DbNumber x) <- dbItems ]
 
 -- It seems like there's a better way to do this but I can't figure out how
-maxUTCTime = (UTCTime (fromGregorian 9999 12 31) (secondsToDiffTime 34123))
+minUTCTime = (UTCTime (fromGregorian 0 1 1) (secondsToDiffTime 34123))
 mostRecent :: [DatabaseItem] -> UTCTime
-mostRecent dbItems = foldr min maxUTCTime (filterDbDate dbItems)
+mostRecent = foldr max minUTCTime . filterDbDate
+
+sumDb :: [DatabaseItem] -> Integer
+sumDb = foldr (+) 0 . filterDbNumber
+
+avgDb :: [DatabaseItem] -> Double
+avgDb dbItems = (fromIntegral (sumDb dbItems)) / (fromIntegral (length (filterDbNumber dbItems)))
