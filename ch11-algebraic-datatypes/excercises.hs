5cbe97e1b099ab27bd4b39b2cc6f9b2edc56e3d8
Ry Wiese <rywiese@mn-rwiese-m.ad.smartthings.com>
2021-04-24 15:57:54 -0500

    11.8

--- ch11-algebraic-datatypes/excercises.hs
+++ ch11-algebraic-datatypes/excercises.hs
@@ -1,1 +1,14 @@
 -- Come back and do 11.5?
+
+
+-- Cardinality
+
+-- 1 1
+
+-- 2 3
+
+-- 3 65536
+
+-- 4 18446744073709551616 == 2 ^ 64
+
+-- 5 2 ^ 8 == 256. 8 bits, each with two possible values (1 or 0), can represent 256 different numbers.
